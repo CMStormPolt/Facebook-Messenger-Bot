1f2f35997c69411839f11334cbd7fabb2b20637c
FBSalesBot <d.irinkovn1@gmail.com>
2017-04-12 17:25:46 +0300

    ghkjk

--- index.js
+++ index.js
@@ -6,11 +6,3 @@ const BotStack = require('./src/botstack.js');
 const ParaBot = new BotStack('ParaBot')
 ParaBot.startServer()
 
-
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0bc63fb918cdd64689586f5507da602f1508f3e0
